5ee1ab99641e7f586d5d118a7ac840a4d04e63e7
mingttong <zhouwunan@qq.com>
2017-05-03 10:45:45 +0800

    在phantom-live-state.js添加了页面加载完房间名判断
    房间名加载出来了才能判断页面各个部分加载完成，
    才能通过判断是否有 “上次开播” 来判断主播是否在直播。

--- phantom-live-state.js
+++ phantom-live-state.js
@@ -19,15 +19,51 @@ if (args.length === 1) {
 
 var url = baseUrl + roomNumber;
 
+/**
+ * @name waitForRoomName
+ * @description 异步，等待房间名加载完成后，执行回调函数。
+ * @param foo       {Function} 回调函数
+ * @param delay     {Number}   延迟时间 可选参数
+ */
+function waitForRoomName(foo, delay) {
+  "use strict";
+
+  // 默认
+  delay = delay === 0 ? delay : (!!delay ? delay : 200);
+
+  setTimeout(function timer() {
+
+    // 如果房间名加载出来了，则执行回调函数
+    if (!!(document.querySelector("h1").textContent)) {
+
+      foo();
+
+    // 如果房间名未加载出来，则继续等待
+    } else {
+      console.log('等待...');
+      setTimeout(timer, delay);
+    }
+
+  }, delay)
+}
+
 page.open(url, function (status) {
   "use strict";
 
-  isLive = page.evaluate(function () {
-    // 如果存在“上次直播”，则说明现在开播
-    return !(document.querySelector('div.time-box'));
+  // 等待房间名加载出来了再去判断是否在直播
+  // 通过判断是否存在元素节点 div.time-box
+  waitForRoomName(function () {
+    isLive = page.evaluate(function () {
+      // 如果存在“上次直播”，则说明现在开播
+      return !(document.querySelector('div.time-box'));
+    });
+
+    page.render('./pics/douyu.png');
+
+    console.log(isLive);
+
+    phantom.exit();
   });
 
-  console.log(isLive);
-
-  phantom.exit();
+  console.log('准备等待房间名加载...')
 });
