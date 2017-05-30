911f7215a7c5cdc11cf375e28b1180be7c51a133
mingttong <zhouwunan@qq.com>
2017-05-30 09:11:18 +0800

    try get follow list

--- server.js
+++ server.js
@@ -2,13 +2,32 @@
  * Created by lenovo on 2017/5/12.
  */
 
-const http = require('http');
-const server = http.createServer( (req, res) => {
-  "use strict";
+const express = require('express');
+const moment = require('moment');
+const morgan = require('morgan');
+const app = express();
+const router = express.Router();
+const cors = require('cors');
 
+app.use(morgan('common'));
+app.use(cors({
+  origin: ['http://localhost:3001'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.get('/', function (req, res) {
+  res.json({status: 'My Api is alive!'});
 });
 
-server.listen(80, function () {
+app.listen(3000, function () {
   "use strict";
-  console.log('now listen port 80...');
-})
+  console.log('3000 My Api is running...');
+});
+
+app.listen(3001, function () {
+  "use strict";
+  console.log('3001 My Api is running...');
+});
+
+module.exports = app;
